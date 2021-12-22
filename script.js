c8411bccd339525ebed960b8cbff727a3ed7ab4b
Michał Wawrzeńczyk <wawrzenczyk.m@yahoo.com>
2021-12-23 00:06:07 +0100

    feat(show-data): create table element, style table, create logic to show data

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 const companies = 'http://localhost:3000/companies';
 const users = 'http://localhost:3000/users';
+const table = document.getElementById('table');
 
 const getCompanies = async () => {                                              // asynchronous (not-blocking code) function which returns a promise.
-    
     const responseCompanies = await fetch(companies);                           // "fetch" returns a promise, "await" stops assigning value to "response" variable, until the promise will be resolved.
     const dataCompanies = await responseCompanies.json();                       // "json" method returns a promise. This give us data as a JS object.
     // console.log(dataCompanies);
-
-    const companyNameArray = [];
-
-    // create array with company names
-    for (i = 0; i < dataCompanies.length; i++) {
-        if (dataCompanies[i].name) {
-            companyNameArray.push(dataCompanies[i].name);
-            // console.log(companyNameArray);
-        };
-    };
-
-    // display list of company names in the browser
-    for (i = 0; i < companyNameArray.length; i++) {
-        const companyListItem = document.createElement('p');
-        companyListItem.innerHTML = companyNameArray[i];
-        document.getElementById('companies').appendChild(companyListItem);
-    };
+    // return array witch company objects
+    return dataCompanies;
 };
-
-getCompanies();
 
 const getUsers = async () => {
     const responseUsers = await fetch(users);
     const dataUsers = await responseUsers.json();
-
-    const companyUsersArray = [];
-
-    for (i = 0; i < dataUsers.length; i++) {
-        if (dataUsers[i].email) {
-            companyUsersArray.push(dataUsers[i].email);
-        };
-    };
-
-    for (i = 0; i < companyUsersArray.length; i++) {
-        const usersListItem = document.createElement('p');
-        usersListItem.innerHTML = companyUsersArray[i];
-        document.getElementById('users').appendChild(usersListItem);
-    };
+    return dataUsers;
 }
 
-getUsers();
+const getData = async () => {
+    const companyData = await getCompanies();
+    const userData = await getUsers();
+
+    companyData.forEach(company => {
+        const row = document.createElement('tr');
+        const companyCell = document.createElement('td')
+        const userCell = document.createElement('td');
+        companyCell.innerHTML = company.name;
+
+        table.appendChild(row);
+        row.appendChild(companyCell);
+        row.appendChild(userCell);
+
+        let result = userData.filter(checkUri).map(userName);
+
+        function checkUri (userDataObject) {
+            return userDataObject.uris.company === company.uri;
+        };
+
+        function userName (userObject) {
+            return userObject.name;
+        };
+
+        userCell.innerText = result;
+    });
+};
+
+getData();
